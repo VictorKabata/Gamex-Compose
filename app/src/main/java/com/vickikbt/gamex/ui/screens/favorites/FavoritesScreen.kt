962db43e946be178aebac62d849f7cc037bfd45a
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2021-10-06 18:47:45 +0300

    feat: Added home, favorites and settings screen templates

---
+++ app/src/main/java/com/vickikbt/gamex/ui/screens/favorites/FavoritesScreen.kt
@@ -0,0 +1,15 @@
+package com.vickikbt.gamex.ui.screens.favorites
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun FavoritesScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    FavoritesScreen()
+}
---
+++ app/src/main/java/com/vickikbt/gamex/ui/screens/settings/SettingsScreen.kt
@@ -0,0 +1,15 @@
+package com.vickikbt.gamex.ui.screens.settings
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SettingsScreen(){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview(){
+    SettingsScreen()
+}
