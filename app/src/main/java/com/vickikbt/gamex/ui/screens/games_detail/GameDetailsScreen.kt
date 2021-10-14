014f69830b59bc546bde8eeca30766c85d03ef16
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2021-10-14 19:26:47 +0300

    fix: Added background image in game details screen

--- app/src/main/java/com/vickikbt/gamex/ui/screens/games_detail/GameDetailsScreen.kt
+++ app/src/main/java/com/vickikbt/gamex/ui/screens/games_detail/GameDetailsScreen.kt
@@ -1,13 +1,46 @@
 package com.vickikbt.gamex.ui.screens.games_detail
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.unit.sp
-import com.vickikbt.gamex.ui.theme.ColorPrimary
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 @Composable
 fun GameDetailsScreen(gameIdArg: Int) {
+    val scrollState = rememberScrollState()
 
-    Text(text = "Game id passed: $gameIdArg", color = ColorPrimary, fontSize = 20.sp)
+    val defaultDominantColor = MaterialTheme.colors.surface
+    val dominantColor = remember { mutableStateOf(defaultDominantColor) }
+    val dominantTextColor = remember { mutableStateOf(defaultDominantColor) }
+
+    /*Column() {
+        val painter = rememberImagePainter(
+            data = game.background_image,
+            builder = { crossfade(true) }
+        )
+
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(220.dp),
+            alignment = Alignment.Center,
+            contentScale = ContentScale.Crop,
+            painter = painter,
+            contentDescription = null
+        )
+
+        if (painter.state is ImagePainter.State.Success) {
+            LaunchedEffect(key1 = painter) {
+                launch {
+                    val imageDrawable = painter.imageLoader.execute(painter.request).drawable
+                    viewModel.getImageDominantSwatch(imageDrawable!!) {
+                        dominantColor.value = Color(it.rgb)
+                        dominantTextColor.value = Color(it.titleTextColor)
+                    }
+                }
+            }
+        }
+    }*/
 
 }
