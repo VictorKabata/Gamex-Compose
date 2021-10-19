590f83b615b526262621ad5389bfb4fd9784cf88
ndiritumichael <ndiritumichael5@gmail.com>
2021-10-19 16:58:52 +0300

    Implemented hiding navigation bar when the current screen is not among  the three top destinations i.e Home,Search,Favorite

--- app/src/main/java/com/vickikbt/gamex/ui/activity/MainActivity.kt
+++ app/src/main/java/com/vickikbt/gamex/ui/activity/MainActivity.kt
@@ -46,15 +46,22 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
+    val navBarItems = listOf(NavigationItem.Home, NavigationItem.Search, NavigationItem.Favorite)
+    val showNavBar = navController
+        .currentBackStackEntryAsState().value?.destination?.route in navBarItems.map {
+        it.route
+    }
 
-    Scaffold(bottomBar = { BottomNavigationBar(navController = navController) }) {
+    Scaffold(bottomBar = { if (showNavBar) {
+        BottomNavigationBar(navController = navController,navBarItems)
+    } }) {
         Navigation(navController = navController)
     }
 }
 
 @Composable
-fun BottomNavigationBar(navController: NavController) {
-    val items = listOf(NavigationItem.Home, NavigationItem.Search, NavigationItem.Favorite)
+fun BottomNavigationBar(navController: NavController,items: List<NavigationItem>) {
+
 
     Card(
         modifier = Modifier
@@ -107,8 +114,9 @@ fun BottomNavigationBar(navController: NavController) {
 @Composable
 fun BottomNavigationPreview() {
     val navController = rememberNavController()
+    val navBarItems = listOf(NavigationItem.Home, NavigationItem.Search, NavigationItem.Favorite)
 
     GamexTheme {
-        BottomNavigationBar(navController)
+        BottomNavigationBar(navController,navBarItems)
     }
 }
